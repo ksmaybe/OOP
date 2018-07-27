751d5bdff5d68c52e19dfc5028b2b2f499b29ff0
ksmaybe <ksmaybe>
2018-07-27 09:18:54 -0400

    split date wokring 1

--- Code/Project1/date.cpp
+++ Code/Project1/date.cpp
@@ -1,29 +1,78 @@
 #include "std_lib_facilities.h"
 #include "date.h"
 
+vector<int> non_leap_day{ 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+vector<int> leap_day{ 0,31,29,31,30,31,30,31,31,30,31,30,31 };
+vector<int> last_day;
+vector<string> mons{ "haha","January","Feburary","March","April","May","June","July","August","September","Ocotober","November","December" };
 
-int main()
+Month operator++(Month& m)
 {
-	int xx;
-	int yy;
-	int zz;
-	string input;
-	cout << "Enter a date in format (YYYY,MM,DD): ";
-	cin >> input;
-	istringstream ss(input);
-	string token_y;
-	string token_m;
-	string token_d;
-	getline(ss, token_y, ',');
-	stringstream y(token_y);
-	y >> xx;
-	getline(ss, token_m, ',');
-	stringstream m(token_m);
-	m >> yy;
-	getline(ss, token_d, ',');
-	stringstream d(token_d);
-	d >> zz;
-	f(xx, yy, zz);
-	keep_window_open();
-	return 0;
+	m = (m == Month::dec) ? Month::jan : Month(int(m) + 1);
+	return m;
+}
+
+Date::Date(int yy, int mm, int dd)
+	:y{ yy }, m{ mm }, d{ dd }
+{
+	if (y % 4 == 0) last_day = leap_day;
+	else
+		last_day = non_leap_day;
+	if (!isvalid())
+		throw Invalid{};
+}
+
+
+
+void Date::add_day(int n)
+{
+	if (n > 10)
+	{
+		add_day(10);
+		add_day(n - 10);
+	}
+	else
+	{
+		d += n;
+	}
+}
+
+bool Date::isvalid()
+{
+	if (int(m) < 1 || int(m) > 12)
+		return false;
+	if (int(d) < 1 || int(d) > last_day[m])
+		return false;
+	if (int(y) < 0)
+		return false;
+	return true;
+}
+const int Date::year()
+{
+	return y;
+}
+const int Date::month()
+{
+	return m;
+}
+const int Date::day()
+{
+	return d;
+}
+
+ostream& operator <<(ostream& os, Date d)
+{
+	return os << '(' << mons[d.month()] << " " << d.day() << "," << d.year() << ")" << endl;
+}
+void f(int x, int y, int z)
+try
+{
+	Date dxy{ x,y,z };
+	cout << dxy << endl;
+	dxy.add_day(2);
+	cout << dxy << endl;
+}
+catch (Date::Invalid)
+{
+	error("invalid date");
 }
--- Code/Project1/date_test.cpp
+++ Code/Project1/date_test.cpp
@@ -1,13 +1,31 @@
+
 #include "std_lib_facilities.h"
-#include "date.h"
+
+
+void f(int x, int y, int z);
 
 int main()
 {
-	int x;
-	int y;
-	int z;
-	cout << "Enter Year_Month_Day: ";
-	cin >> x >> y >> z;
-	f(x, y, z);
-	
+	int xx;
+	int yy;
+	int zz;
+	string input;
+	cout << "Enter a date in format (YYYY,MM,DD): ";
+	cin >> input;
+	istringstream ss(input);
+	string token_y;
+	string token_m;
+	string token_d;
+	getline(ss, token_y, ',');
+	stringstream y(token_y);
+	y >> xx;
+	getline(ss, token_m, ',');
+	stringstream m(token_m);
+	m >> yy;
+	getline(ss, token_d, ',');
+	stringstream d(token_d);
+	d >> zz;
+	f(xx, yy, zz);
+	keep_window_open();
+	return 0;
 }
