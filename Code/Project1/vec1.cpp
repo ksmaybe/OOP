8a4b62b947610f311a6aa60a4cf829cf5817132d
Mr Grey <ksmaybe@gmail.com>
2018-08-03 13:02:24 -0400

    vect

---
+++ Code/Project1/vec1.cpp
@@ -0,0 +1,51 @@
+#include "std_lib_facilities.h"
+
+class vect
+{
+	int sz;
+	double* elem;
+public:
+	vect(int s)
+		:sz{ s }, elem{ new double[s] } {} /*{
+		for (int i = 0; i < sz; ++i) elem[i] = 0.0;
+	}
+	vect(initializer_list<double> lst)
+		:sz{lst.size()},elem{new double[sz]}
+	{
+		copy(lst.begin(), lst.end(), elem);
+	}*/
+	~vect()
+	{
+		delete[] elem;
+	}
+	double get(int i);
+	void set(int i, double d);
+};
+
+double vect::get(int i)
+{
+	return elem[i];
+}
+
+void vect::set(int i, double d)
+{
+	elem[i] = d;
+}
+
+void f(int n)
+{
+	vect v(3);
+	v.set(2, 2.2);
+	vect v2 = v;
+	v.set(1, 9.9);
+	v2.set(0,8.8);
+	cout << v.get(0) << ' ' << v.get(1) << ' ' <<v.get(2) <<endl;
+	cout << v2.get(0) << ' ' << v2.get(1) << ' ' << v2.get(2) << endl;
+}
+
+int main()
+{
+	f(3);
+	keep_window_open();
+	return 0;
+}
---
+++ Code/Project1/vec2.cpp
@@ -0,0 +1,69 @@
+#include "std_lib_facilities.h"
+
+class vect
+{
+	int sz;
+	double* elem;
+public:
+	vect(const vect& arg);
+	vect & operator=(const vect& a);
+	vect(int s)
+		:sz{ s }, elem{ new double[s] } {} /*{
+										   for (int i = 0; i < sz; ++i) elem[i] = 0.0;
+										   }
+										   vect(initializer_list<double> lst)
+										   :sz{lst.size()},elem{new double[sz]}
+										   {
+										   copy(lst.begin(), lst.end(), elem);
+										   }*/
+	~vect()
+	{
+		delete[] elem;
+	}
+	double get(int i);
+	void set(int i, double d);
+};
+
+double vect::get(int i)
+{
+	return elem[i];
+}
+
+void vect::set(int i, double d)
+{
+	elem[i] = d;
+}
+
+vect::vect(const vect& arg)
+	:sz{arg.sz},elem{new double[arg.sz]}
+{
+	copy(arg.elem,arg.elem+arg.sz,elem);
+}
+
+vect& vect::operator=(const vect& a)
+{
+	double* p = new double[a.sz];
+	copy(a.elem, a.elem+a.sz, p);
+	delete[] elem;
+	elem = p;
+	sz = a.sz;
+	return *this;
+}
+
+void f(int n)
+{
+	vect v(3);
+	v.set(2, 2.2);
+	vect v2 = v;
+	v.set(1, 9.9);
+	v2.set(0, 8.8);
+	cout << v.get(0) << ' ' << v.get(1) << ' ' << v.get(2) << endl;
+	cout << v2.get(0) << ' ' << v2.get(1) << ' ' << v2.get(2) << endl;
+}
+
+int main()
+{
+	f(3);
+	keep_window_open();
+	return 0;
+}
